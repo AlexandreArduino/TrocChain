6a5eaf70dc8326423dbba814adc9c8f5fb351cef
= <=>
2022-03-23 18:00:04 +0100

    added user id creation function

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include "debug/log.h"
 #include "object/object.h"
 #include "library/pictures/resize.h"
+#include "identity/identity.h"
 
 int main() {
     logger.enable();
-    ObjectCreation::generate_object_template("assets/examples/linux.jpeg", {"Tux", "Logo", "Picture"});
+    // ObjectCreation::generate_object_template("assets/examples/linux.jpeg", {"Tux", "Logo", "Picture"});
+    IdentityCreation::create_new_identity("admin"); 
     return 0;
 }
